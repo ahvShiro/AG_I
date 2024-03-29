b0f462afeead7785400c5fe338a1183e6ec8df28
Arthur Shiro <arthurshiro123@gmail.com>
2024-03-29 00:03:55 -0300

    Criado arquivo para a lista 04

---
+++ atividade04.py
@@ -0,0 +1,10 @@
+
+
+print("01. Construa um programa que mostre menu exatamente como o exemplo abaixo e implemente o código necessário para seu funcionamento")
+
+# == Menu de Opções ==
+# 	1. Somar 2 números
+# 	2. Potência Y de um número X (xy)
+# 3. Raiz quadrada de X
+# 4. Gerar um número aleatório
+# == Opção escolhida:
