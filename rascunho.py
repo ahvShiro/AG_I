fe9bb677e1fdc0825d629a4f3ed4262a28a2e565
ahvShiro <arthurshiro123@gmail.com>
2024-07-30 20:37:25 -0300

    Atividade proposta pelo professor Ayslan em sala]

--- rascunho.py
+++ rascunho.py
@@ -1,29 +1,46 @@
 
 
-# for i in range(altura):
-#     # # SPACING
-#     # for spacing in range(altura - j - 1):
-#     #     print(" ", end="")
-#     # # ROW
-#     for j in range(i + 1):
-#         print("#", end="")
-#     # LINE BREAK
+# # for i in range(altura):
+# #     # # SPACING
+# #     # for spacing in range(altura - j - 1):
+# #     #     print(" ", end="")
+# #     # # ROW
+# #     for j in range(i + 1):
+# #         print("#", end="")
+# #     # LINE BREAK
+# #     print()
+
+
+
+# altura = int(input("altura: "))
+
+# i = 0
+# while i < altura:
+#     j = 0
+#     while j <= i:
+#         print("#", end='')
+#         j += 1
 #     print()
+#     i += 1
 
 
+# 30/07/2024 AG_I #######################################################
 
-altura = int(input("altura: "))
+# preco = 100
+# juros = 0.01
 
-i = 0
-while i < altura:
-    j = 0
-    while j <= i:
-        print("#", end='')
-        j += 1
-    print()
-    i += 1
+juros = float(input("Digite os juros em decimal: "))
+valorInicial = float(input("Digite o valor inicial: "))
+valorFinal = float(input("Digite o valor final: "))
 
+mes = 0
+preco = valorInicial
 
+while (preco <= valorFinal):
+    mes += 1
+    preco += preco * juros
+
+print(f"{int(mes / 12)} anos e {mes % 12} meses para R${valorInicial:.2f} virarem R${valorFinal:.2f}")
 
 
 
