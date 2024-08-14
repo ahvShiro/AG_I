ce419b255871b213c28302d4b672c53b850aa456
ahvShiro <arthurshiro123@gmail.com>
2024-08-13 22:43:33 -0300

    slaa

---
+++ 06. For Loop.py
@@ -0,0 +1,8 @@
+# 1) Faça um programa em Python utilizando o for (um programa pra cada um), que:
+# a) Apresente os números de 1 a 100 (um por linha).
+# b) Apresente os números de 100 a 1 (um por linha).
+# c) Apresente os números pares de 1 a 100 (um por linha).
+# d) Apresente os números ímpares de 1 a 100 (um por linha).
+# e) Faça a soma dos números de 1 a 100 e ao final mostre apenas a soma total.
+# f) Faça a soma dos números de X a Y (informados pelo usuário), desde que X seja menor que Y, e apresente o valor total (semelhante ao anterior).
+# g) Faça a multiplicação dos números de 1 a j (fatorial) e mostre o resultado final. Exemplo: Se j = 5 você deve calcular 1 * 2 * 3 * 4 * 5 = 120
