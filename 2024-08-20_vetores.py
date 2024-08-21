87b59b58eb0f320da3d94e855691e8984565645d
ahvShiro <arthurshiro123@gmail.com>
2024-08-20 22:33:20 -0300

    2024-08-20 10:32:50 Vetores

--- 2024-08-20_vetores.py
+++ 2024-08-20_vetores.py
@@ -1,6 +1,31 @@
-size = int(input("Insira o tamanho do vetor: "))
+# size = int(input("Insira o tamanho do vetor: "))
+# vector = [0] * size
+# print(vector)
 
-vector = [0] * size
+##################################################################
 
+# alunos = [""] * 30
+# alunos[0] = "Arthur Heiji Voos Shiroshima"
+
+# i = int(input("Qual posição você quer acessar? "))
+# alunos[i] = input("Qual é o seu nome? ")
+
+# print(alunos)
+
+# t = len(alunos)
+# print(t)
+
+
+
+
+vector = [0] * 10
+
+while True:
+    i = int(input("Digite um endereço para inserir um valor: "))
+    if(i < 0 or i > (len(vector) - 1)):
+        print(f"Valor {i} inválido, tente novamente")
+    else:
+        break
+
+vector[i] = int(input("Digite um valor: "))
 print(vector)
-
