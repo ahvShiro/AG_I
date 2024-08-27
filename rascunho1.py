b9fc3607306ea8e6d888e215b48fd4e0629420c8
ahvShiro <arthurshiro123@gmail.com>
2024-08-27 20:01:47 -0300

    Anotações feitas na sala ate as 20h

--- rascunho1.py
+++ rascunho1.py
@@ -1,19 +1,19 @@
-import random
+# import random
 
-even_nums = 0
-odd_nums = 0
-multiple_of_three = 0
+# even_nums = 0
+# odd_nums = 0
+# multiple_of_three = 0
 
-total_sum = 0
-average = 0
-iterations = 0
+# total_sum = 0
+# average = 0
+# iterations = 0
 
-for i in range(30):
-    num = random.randint(0, 100)
-    if(num == 50):
-        break
-    if(num % 5 == 0):
-        continue
+# for i in range(30):
+#     num = random.randint(0, 100)
+#     if(num == 50):
+#         break
+#     if(num % 5 == 0):
+#         continue
 
 
 # average = round((total_sum / iterations), 2)
@@ -23,3 +23,18 @@ for i in range(30):
 # print(f"Múltiplos de 3: {multiple_of_three}")
 # print(f"Soma de todos: {total_sum}")
 # print(f"Média: {average}")
+
+
+
+
+x = [0] * 5
+
+print(len(x))
+
+nome = input("string: ")
+
+print(len(nome))
+
+print(x[0])
+print(nome[len(nome) - 1])
+
