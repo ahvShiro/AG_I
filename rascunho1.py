ad9d7001221b882a7f0d2cca4295f9b7aae2feb7
ahvShiro <arthurshiro123@gmail.com>
2024-08-27 22:49:11 -0300

    2024-08-27 22:48:48 exercicio em sala incompleto

--- rascunho1.py
+++ rascunho1.py
@@ -40,32 +40,59 @@ import time
 # print(nome[len(nome) - 1])
 
 
-tamanho_vetor = int(input("Digite a quantidade de alunos na sala: "))
+# tamanho_vetor = int(input("Digite a quantidade de alunos na sala: "))
 
-chamada = [" "] * tamanho_vetor
-for i in range(0, len(chamada)):
-    chamada[i] = input("Seu nome: ")
+# chamada = [" "] * tamanho_vetor
+# for i in range(0, len(chamada)):
+#     chamada[i] = input("Seu nome: ")
 
-print("== Alunos ==")
+# print("== Alunos ==")
 
-for nome in chamada:
-    print(nome)
+# for nome in chamada:
+#     print(nome)
 
-random.shuffle(chamada)
+# random.shuffle(chamada)
 
-print("============")
-
-
-qt_premios = int(input("Quantos prêmios serão sorteados? "))
+# print("============")
 
 
-for i in range(1, qt_premios + 1):
-    input(f"O {i}º sorteio será realizado. Pressione enter para prosseguir... ")
+# qt_premios = int(input("Quantos prêmios serão sorteados? "))
 
-    for i in range(5, 0, -1):
-        print(f"{i}...")
-        time.sleep(1)
 
-    aluno_sorteado = random.randint(0, len(chamada) - 1)
+# for i in range(1, qt_premios + 1):
+#     input(f"O {i}º sorteio será realizado. Pressione enter para prosseguir... ")
 
-    print(f"Parabéns {chamada[aluno_sorteado]}, você ganhou um prêmio surpresa!")
+#     for i in range(5, 0, -1):
+#         print(f"{i}...")
+#         time.sleep(1)
+
+#     aluno_sorteado = random.randint(0, len(chamada) - 1)
+
+#     print(f"Parabéns {chamada[aluno_sorteado]}, você ganhou um prêmio surpresa!")
+
+
+
+
+quantidade_times = int(input("Quantidade de times: "))
+
+times = [" "] * quantidade_times
+pontuacoes = [0] * quantidade_times
+
+primeiro_maior_pontuacao = 0
+primeiro_maior_indice = 0
+
+segundo_maior_pontuacao = 0
+segundo_maior_indice = 0
+
+for i in range(0, quantidade_times):
+    times[i] = input("Nome: ")
+    pontuacoes[i] = int(input("Pontuação: "))
+
+    if(times[i] > primeiro_maior_pontuacao):
+        primeiro_maior_indice = i
+    elif(times[i] < primeiro_maior_pontuacao and times[i] > segundo_maior_pontuacao):
+        segundo_maior_indice = i
+
+
+print(f"Campeão: {times[primeiro_maior_indice]} com {pontuacoes[primeiro_maior_indice]}")
+print(f"Vice-Campeão: {times[segundo_maior_indice]} com {pontuacoes[segundo_maior_indice]}")
