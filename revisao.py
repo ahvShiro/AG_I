d42462df8b04685397e1f512a66fcbfbcbb7d9bd
ahvShiro <arthurshiro123@gmail.com>
2024-10-08 20:55:00 -0300

    revisão pra prova

---
+++ revisao.py
@@ -0,0 +1,68 @@
+# # Bubble sort
+# vetor = [3, 2, 5, 4, 1]
+# print(vetor)
+
+# # Itera um ciclo
+# for i in range(0, len(vetor) - 1):
+#     # Itera dentro do vetor
+#     for j in range(0, len(vetor) - 1):
+#         # Se o primeiro for maior que o segundo
+#         if(vetor[j] > vetor[j + 1]):
+#             # Troca
+#             x = vetor[j]
+#             vetor[j] = vetor[j + 1]
+#             vetor[j + 1] = x
+
+# print(vetor)
+
+
+# # Selection Sort
+# vetor = [3, 2, 5, 4, 1]
+# print(vetor)
+
+
+
+
+
+# # Insertion Sort
+# vetor = [3, 2, 5, 4, 1]
+# print(vetor)
+
+
+
+
+
+
+# # Busca Sequêncial
+# vetor = [3, 2, 5, 4, 1]
+# vetor.sort()
+# print(vetor)
+
+# valor = int(input("Valor a ser buscado: "))
+# foiEncontrado = False
+
+# for i in range(0, len(vetor)):
+#     if(valor == vetor[i]):
+#         foiEncontrado = True
+#         break
+#     elif(valor < vetor[i]):
+#         break
+
+# if(foiEncontrado):
+#     print(f"Valor {valor} encontrado")
+# else:
+#     print(f"Valor {valor} não encontrado")
+
+
+
+
+# Busca Binária
+vetor = [3, 2, 5, 4, 1]
+vetor.sort()
+print(vetor)
+
+valor = int(input("Valor a ser buscado: "))
+foiEncontrado = False
+
+
+
