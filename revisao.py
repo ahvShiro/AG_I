33bcde2613311ca3f3118cd0112c4e35df82a7d3
ahvShiro <arthurshiro123@gmail.com>
2024-10-14 19:14:37 -0300

    aaaaAAAAAAAAAAA

--- revisao.py
+++ revisao.py
@@ -1,45 +1,92 @@
-# # Bubble sort
-# vetor = [3, 2, 5, 4, 1]
-# print(vetor)
+def bubbleSort(vetor):
+    # Itera um ciclo
+    for i in range(0, len(vetor) - 1):
+        print(f"== Ciclo {i+1} =======")
 
-# # Itera um ciclo
-# for i in range(0, len(vetor) - 1):
-#     # Itera dentro do vetor
-#     for j in range(0, len(vetor) - 1):
-#         # Se o primeiro for maior que o segundo
-#         if(vetor[j] > vetor[j + 1]):
-#             # Troca
-#             x = vetor[j]
-#             vetor[j] = vetor[j + 1]
-#             vetor[j + 1] = x
+        # Itera dentro do vetor
+        for j in range(0, len(vetor) - 1 - i):
+            print(vetor)
 
-# print(vetor)
+            # Se o primeiro for maior que o segundo
+            if(vetor[j] > vetor[j + 1]):
+                print(f"Trocando {vetor[j]} com {vetor[j + 1]}")
+
+                # Troca
+                x = vetor[j]
+                vetor[j] = vetor[j + 1]
+                vetor[j + 1] = x
+
+    return(vetor)
+
+def selectionSort(vetor):
+    # Itera um ciclo
+    for i in range(0, len(vetor)):
+        print(f"== Ciclo {i + 1} =======")
+        print(f"Estado atual do vetor: {vetor}")
+
+        # Define o indice do menor valor encontrado até agr
+        indiceMenorValor = i
+
+        # Descobre o indice do menor valor no vetor
+        for j in range(i + 1, len(vetor)):
+            print(f"Comparando {vetor[j]} com {vetor[indiceMenorValor]}")
+            if vetor[j] < vetor[indiceMenorValor]:
+                indiceMenorValor = j
+
+        # Imprime o indice do menor valor encontrado
+        print(f"Menor valor encontrado: {vetor[indiceMenorValor]} na posição [{indiceMenorValor}]")
+
+        # Confere se o elemento atual não é o menor
+        if(i != indiceMenorValor):
+            
+            # Troca
+            print(f"Trocando {vetor[i]} com {vetor[indiceMenorValor]}")
+            x = vetor[j]
+            vetor[j] = vetor[j + 1]
+            vetor[j + 1] = x
+        
+    return vetor
 
 
-# # Selection Sort
-# vetor = [3, 2, 5, 4, 1]
-# print(vetor)
+def insertionSort(vetor):
+    return(vetor)
+
+
+
+tam = int(input("== Insira o tamanho do vetor ==\n"))
+vetor = [0] * tam
+for i in range(0, len(vetor)):
+    vetor[i] = int(input(f"Valor da posição [{i}]: "))
+
+
+
+ans = int(input("== Selecione um algoritmo ==\n1. Bubble Sort 🫧\n2. Selection Sort ⤵️\n3. Insertion Sort 🃏\n"))
+
+match ans:
+    case 1:
+        bubbleSort(vetor)
+    case 2:
+        selectionSort(vetor)
+    case 3:
+        insertionSort(vetor)
+    case _:
+        print("Digite um valor válido")
+
+
+print(f"Vetor ordenado: {vetor}")
 
 
 
 
+exit()
 
-# # Insertion Sort
-# vetor = [3, 2, 5, 4, 1]
-# print(vetor)
+# Busca Sequêncial
+vetor = [3, 2, 5, 4, 1]
+vetor.sort()
+print(vetor)
 
-
-
-
-
-
-# # Busca Sequêncial
-# vetor = [3, 2, 5, 4, 1]
-# vetor.sort()
-# print(vetor)
-
-# valor = int(input("Valor a ser buscado: "))
-# foiEncontrado = False
+valor = int(input("Valor a ser buscado: "))
+foiEncontrado = False
 
 for i in range(0, len(vetor)):
     if(valor == vetor[i]):
@@ -48,19 +95,15 @@ for i in range(0, len(vetor)):
     elif(valor < vetor[i]):
         break
 
-# if(foiEncontrado):
-#     print(f"Valor {valor} encontrado")
-# else:
-#     print(f"Valor {valor} não encontrado")
+if(foiEncontrado):
+    print(f"Valor {valor} encontrado")
+else:
+    print(f"Valor {valor} não encontrado")
 
 
 
 
 # Busca Binária
-vetor = [3, 2, 5, 4, 1]
-vetor.sort()
-print(vetor)
-
 foiEncontrado = False
 valor = int(input("Valor a ser buscado: "))
 inicio = 0
