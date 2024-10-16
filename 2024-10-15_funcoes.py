25f959f72922a11c586681f52d12f0b8971d900e
ahvShiro <arthurshiro123@gmail.com>
2024-10-15 22:08:44 -0300

    renomeado

---
+++ 2024-10-15_funcoes.py
@@ -0,0 +1,24 @@
+import time
+
+# # Funções Built-In
+# print(f"print()")
+
+# x = int(input("x: "))
+
+
+# # Funções de Bibliotecas
+# import random
+# x = random.randint(1, 10)
+
+# import math
+# x = math.sqrt(x)
+
+
+# Funções de Usuários
+def helloWorld():
+    print("Função invocada 😈")
+    time.sleep(5)
+    print("Hello, world! :)")
+    print("Uma função é usada para reutilizar trechos de código")
+
+helloWorld()
